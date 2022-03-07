77c9c4d677d321266a40d32e93f3b6599d49f73a
Edison <edison@MacBook-Pro-xt.local>
2022-03-07 23:52:41 +0800

    feat: 微应用

---
+++ micro-app-vue/src/pages/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-card title="Vue 微应用主页">
+    <h2 class="old-title">主页</h2>
+  </a-card>
+</template>
+<script>
+export default {
+  name: "Home"
+}
+</script>
+<style lang="less" scoped>
+.old-title {
+  font-size: 26px;
+  color: #fb4487;
+}
+</style>
---
+++ micro-app-vue/src/pages/list/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <a-card title="Vue 微应用列表页">
+      <h2 class="old-title">子应用列表页</h2>
+    </a-card>
+  </section>
+</template>
+
+<style lang="less" scoped>
+.old-title {
+  font-size: 26px;
+  color: #fb4487;
+}
+</style>
